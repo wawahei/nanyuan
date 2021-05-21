84df75448fc3632ad124013c4a84450c406aba43
yanghailang <yanghailang@newbeiyang.com>
2021-05-21 16:01:13 +0800

    jwt test

--- src/test/java/com/wawahei/kk/demo/DemoApplicationTests.java
+++ src/test/java/com/wawahei/kk/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.wawahei.kk.demo;
 
 import com.wawahei.kk.demo.nanyuan.DemoApplication;
+import com.wawahei.kk.demo.nanyuan.common.util.JwtUtils;
 import com.wawahei.kk.demo.nanyuan.entity.Dict;
 import com.wawahei.kk.demo.nanyuan.service.impl.DictServiceImpl;
 import org.junit.Test;
@@ -22,6 +23,19 @@ public class DemoApplicationTests {
 	public void test1() {
 		List<Dict> list = dictService.list(null);
 		System.out.println(list.size());
+	}
+
+	@Test
+	public void test2(){
+		String token = JwtUtils.createToken(1L,"wawahei");
+		System.out.println(token);
+	}
+
+	@Test
+	public void test3(){
+		String token = "eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJSKk8sT8xIzdTNyU_PzFPSUUqtKFCyMjQzMjQzNzAyttRRKi1OLfJMAYpBmH6JuakIbUq1AJlNL9NIAAAA._fYUH-xad-SKdckgYH_TOaszGgNHIqnDG7PByqCylJpDjlEWnI4-EdqaQ4Iv3IRr4HYCtH8-lQKyqu5i6cur3Q";
+		boolean valid = JwtUtils.checkToken(token);
+		System.out.println(valid);
 	}
 
 }
